f93e511f7534ec3502acdf7bd213646e7b786b90
TiagoAr <tlolivira970@gmail.com>
2025-05-16 02:46:22 -0300

    feat: criado UsuarioModel

---
+++ src/main/java/springboot/grupoextra/SiteEmpresarialApplication.java
@@ -0,0 +1,13 @@
+package springboot.grupoextra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteEmpresarialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteEmpresarialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot/grupoextra/SiteEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.grupoextra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteEmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
